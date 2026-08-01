00404c3f79d7f8efa25ff6751f7ad32e2f1246ff
D4rkgh057 <danielfuelpaz@outlook.com>
2026-08-01 10:56:52 -0500

    chore(build): run toolchain through pnpm exec

--- skill-visualizer-global/build.bat
+++ skill-visualizer-global/build.bat
@@ -1,5 +1,5 @@
 @echo off
-..\node_modules\.bin\esbuild app.tsx --bundle --external:assert --outfile=bundle.js && ^
-..\node_modules\.bin\unassert bundle.js > bundle.2.js && ^
-..\node_modules\.bin\esbuild bundle.2.js --minify --outfile=bundle.js && ^
+pnpm exec esbuild app.tsx --bundle --external:assert --outfile=bundle.js && ^
+pnpm exec unassert bundle.js > bundle.2.js && ^
+pnpm exec esbuild bundle.2.js --minify --outfile=bundle.js && ^
 del bundle.2.js
